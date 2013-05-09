e28a0c3d0c5ab27c3eea48c74a973060a5761939
olizilla <oli@zilla.org.uk>
2013-05-09 18:22:24 +0100

    Adds moment.js hot sauce

--- client/meteor-london.js
+++ client/meteor-london.js
@@ -2,6 +2,22 @@ Meteor.startup(function(){
   console.log('Meteor London is alive.');
 });
 
-Template.meetupEvent.nextEvent = function(){
-  return Events.findOne();
+Template.upcomingMeetup.events = function(){
+  return Events.find({time: { $gt: Date.now() }}).fetch();
+};
+
+Template.previousMeetup.events = function(){
+  return Events.find({time: { $lt: Date.now() }}).fetch();
+};
+
+Template.upcomingMeetup.calandarFormat = function(ms){
+	return moment(ms).calendar();
+};
+
+Template.upcomingMeetup.dateTimeFormat = function(ms){
+	return moment(ms).format('MMMM Do YYYY, h:mm:ss a');
+};
+
+Template.previousMeetup.dateFormat = function(ms){
+	return moment(ms).format('MMMM Do YYYY');
 };
