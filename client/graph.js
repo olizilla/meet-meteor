c64d3394784b2d7f6dd5263dbf75b953169b1643
olizilla <oli@zilla.org.uk>
2013-05-14 18:38:52 +0100

    Improves x-axis scale ticks and formatting .There is a Date formatting issue on FF

--- client/graph.js
+++ client/graph.js
@@ -2,17 +2,19 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
-var x = d3.time.scale().range([0, width]);
+var x = d3.time.scale().range([0, width])
+x.tickFormat('%B');
 
 var y = d3.scale.linear().range([height, 0]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom")
+    .ticks(d3.time.months, 1)
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left");
+    .orient("left")
 
 var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
